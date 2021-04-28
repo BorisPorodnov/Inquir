f5ef65389bc51862c484670ad722771a97ac0938
Porodnov Boris <porodnov.boris@mail.ru>
2021-04-28 22:44:57 +0700

    fixes QuizController

--- src/main/java/net/porodnov/inquirer/rest/QuizController.java
+++ src/main/java/net/porodnov/inquirer/rest/QuizController.java
@@ -3,7 +3,6 @@ package net.porodnov.inquirer.rest;
 import net.porodnov.inquirer.model.Quiz;
 import net.porodnov.inquirer.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,22 +14,22 @@ public class QuizController {
     @Autowired
     private QuizService quizService;
 
-    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "")
     public List<Quiz> getAll() {
         return quizService.getALL();
     }
 
-    @RequestMapping(value = "/post", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/adding")
     public Quiz saveQuiz(@RequestBody Quiz quiz) {
         return this.quizService.save(quiz);
     }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "{id}")
     public void deleteById(@PathVariable("id") Long id) {
         quizService.delete(id);
     }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "{id}")
     public Quiz updateQuiz(@PathVariable Long id, @RequestBody Quiz quiz) {
         return quizService.updateQuiz(id, quiz);
     }
